239bd099aad8773d7342608c65d7ac6a563c8774
Leo Esson <leo.esson@yahoo.com>
2020-02-15 14:54:30 +0100

    Made author method

--- Assignment1B/Program.cs
+++ Assignment1B/Program.cs
@@ -10,12 +10,35 @@ namespace Bookshelf
     {
         static void Main(string[] args)
         {
+            Book bookObj = new Book(); //create an object of class Book
+
+            bookObj.Start(); //use object Book
+            bookObj.ReadAuthor();  //ask for author
         }//main
     }//class shelf
     
     class Book
     {
+        private string author; //books author
+        private int pubYear; //year of publication
+        private bool isFiction; //true if fiction false if nonfiction
 
-    }//class book
+        public void Start()
+        {
+            Console.WriteLine(); //gives blank line
+            Console.WriteLine("Welcome to your library!"); //greetings from this project
+            Console.WriteLine(); //gives blank line
+
+        }//start
+
+        public void ReadAuthor()
+        {
+            Console.WriteLine(*******************); //gives dividing line
+            Console.WriteLine("Please enter the book's author     "); //asks author
+            author = Console.ReadLine(); //sets author value
+        }
+
+
+        }//class book
 
 }//namespace
